bb99a0c48b3d148eebd28f0d5dc44ee626e7cf40
a-moharil <93342024+a-moharil@users.noreply.github.com>
2024-08-04 00:59:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def load_data(file_path):
     return pd.read_csv(file_path)
 
 app = dash.Dash(__name__)
+server = app.server  # Expose the server variable for gunicorn
 df = load_data('players.csv')
 
 app.layout = html.Div([
